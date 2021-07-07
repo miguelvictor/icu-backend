a7229fff3a1a74171407dfaa73890852656bc196
Miguel Victor <miguelvictor.remulta@outlook.com>
2021-07-07 10:47:20 +0800

    Added dashboard APIs

--- predictors/urls.py
+++ predictors/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import get_dashboard_info, get_dashboard_graph
+from .views import get_dashboard_info, get_dashboard_graph, get_dashboard_patients
 
 urlpatterns = [
     path("dashboard-info/", get_dashboard_info),
     path("dashboard-graph/", get_dashboard_graph),
+    path("dashboard-patients/", get_dashboard_patients),
 ]
--- predictors/views.py
+++ predictors/views.py
@@ -1,12 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from icu.models import Patient
+
 import numpy as np
 import random
 
 
 @api_view(["GET"])
-def get_dashboard_info(request):
+def get_dashboard_info(_):
     patients = random.randint(100, 1000)
     icu_patients = random.randint(100, 400)
     warnings = random.randint(100, 200)
@@ -31,11 +33,27 @@ def get_dashboard_info(request):
 
 
 @api_view(["GET"])
-def get_dashboard_graph(request):
+def get_dashboard_graph(_):
     return Response(
         {
             "patients": np.random.gamma(1, 2, 14).tolist(),
             "icu_patients": np.random.gamma(1, 2, 14).tolist(),
             "discharged_patients": np.random.gamma(1, 2, 14).tolist(),
         }
+    )
+
+
+@api_view(["GET"])
+def get_dashboard_patients(_):
+    return Response(
+        [
+            {
+                "subject_id": patient.subject_id,
+                "name": patient.name,
+                "ward_id": random.randint(101, 999),
+                "duration": random.randint(10, 50),
+                "is_high_risk": bool(random.getrandbits(1)),
+            }
+            for patient in Patient.objects.all()[:10]
+        ]
     )
